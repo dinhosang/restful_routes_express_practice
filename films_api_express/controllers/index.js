302f1c6dfd48ad66089c77dc27c464d6c7dbff82
Douglas Sangster <d.sangster@hotmail.co.uk>
2018-02-27 10:02:07 +0000

    adding in route to seeing html and webpack business as part of class work

--- films_api_express/controllers/index.js
+++ films_api_express/controllers/index.js
@@ -4,4 +4,12 @@ const router  = new express.Router()
 router.use('/api/films', require('./apiFilmsController.js'))
 router.use('/reviews', require('./reviewsController.js'))
 
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/../client/build/index.html'));
+});
+
+router.get('/about', function(req, res){
+  res.json({data: "All about us!"});
+})
+
 module.exports = router;
